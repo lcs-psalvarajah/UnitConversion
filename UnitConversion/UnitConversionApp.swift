bb580bd1281ea772cd8f3f56cfd99e63678e1d96
jina <psalvarajah@lcs.on.ca>
2020-11-05 08:40:08 -0500

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Salvarajah, Prajina on 2020-11-05.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

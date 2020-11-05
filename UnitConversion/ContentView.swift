f3deef96c74e3dad0ba85d46841c70ea70c3940c
jina <psalvarajah@lcs.on.ca>
2020-11-05 10:02:00 -0500

    Finished making the sections for the app. There's the user input text field, 2 pickers, and the output text. However I have to work on the picker since both of them are connected and it doesn't allow me to select different units. Also I have to work on the math part.

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -7,10 +7,85 @@
 
 import SwiftUI
 
+
+// Volume Conversion
+
 struct ContentView: View {
+    
+    @State private var unitAmount = ""
+    @State private var unitType = 2
+    @State private var convertedUnit = 2
+    
+    let unitTypes = [ "Milliliters", "Liters" , "Cups" , "Pints" , "Gallons" ]
+    let unitTypesTwo = [ "Milliliters", "Liters" , "Cups" , "Pints" , "Gallons" ]
+
+    
+    var outputValue: Double {
+        let unitValue = Double(unitAmount) ?? 0
+        let firstUnitSelection = Double(unitTypes[unitType])
+    
+        
+        let unitLiters = Measurement(value: unitValue, unit: UnitVolume.liters)
+        let litersToML = unitLiters.converted(to: .milliliters)
+       
+        let unitCups = Measurement(value: unitValue, unit: UnitVolume.cups)
+        let cupsToML = unitCups.converted(to: .milliliters)
+        
+        let unitPints = Measurement(value: unitValue, unit: UnitVolume.pints)
+        let pintsToML = unitPints.converted(to: .milliliters)
+        
+        let unitGallons = Measurement(value: unitValue, unit: UnitVolume.gallons)
+        let gallonsToML = unitGallons.converted(to: .milliliters)
+        
+        
+        
+        
+        
+        return 0
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $unitAmount)
+                        .keyboardType(.decimalPad)
+                }
+                Section (header: Text("What is your original amount unit?")){
+                    Picker("Type of Unit", selection:
+                            $unitType) {
+                        ForEach (0 ..< unitTypes.count) {
+                            Text("\(self.unitTypes[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section (header: Text("What unit do you want to convert to?")){
+                    Picker("Type of Unit", selection:
+                            $unitType) {
+                        ForEach (0 ..< unitTypesTwo.count) {
+                            Text("\(self.unitTypesTwo[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                // the text will change to someing once the math is done
+                Section (header: Text("Converted Value")){
+                    Text ("\(convertedUnit)")
+                }
+            }
+            .navigationBarTitle("Volume Conversion", displayMode: .inline)
+        }
     }
 }
 
